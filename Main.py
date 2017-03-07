72bcaa88031808f84e9a8f4ea1b85e5a3d102d5e
Patrick Piwowarczyk <piwowarczykpatrick@gmail.com>
2017-03-07 02:35:47 -0600

    Finding all image sources

--- Main.py
+++ Main.py
@@ -4,12 +4,12 @@ import re
 
 #Input tags to analyze and scrape
 tagsToSearch = 'drift'
-noOfNotes = []
 counter = 0
 avgNotes = 100
-indexOfSavedPost = []
+indexOfPopularPosts = []
 j = 0
 listOfNotes = []
+imgLinks = []
 
 #searching the tags above
 urlToSearch =  urllib.request.urlopen('https://www.tumblr.com/search/' + tagsToSearch).read()
@@ -26,7 +26,16 @@ for article in r:
 
 
 #Seperating popular content from content we don't want
-for j in noOfNotes:
-    if (noOfNotes[j] > avgNotes):
-        indexOfSavedPost.append(j)
-        print (j,noOfNotes[j])
+for j in range(len(listOfNotes)):
+    if (listOfNotes[j] > avgNotes):
+        indexOfPopularPosts.append(j)
+        print (j,listOfNotes[j])
+
+
+#Downloading popular photos
+allImages = soup.find_all('img')
+for img in allImages:
+    if hasattr(img, 'attrs') and 'src' in img.attrs:
+        if (str(img.attrs['src'])[0:10] == ('https://68')):
+            imgLinks.append(str(img.attrs['src']))
+print(imgLinks)
