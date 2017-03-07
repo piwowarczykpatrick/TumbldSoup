ebb77df578697122581f2fb2949632880bd00a98
Patrick Piwowarczyk <piwowarczykpatrick@gmail.com>
2017-03-06 21:50:43 -0600

    Grabbing value of the popularity of each post.

--- Main.py
+++ Main.py
@@ -4,14 +4,29 @@ import re
 
 #Input tags to analyze and scrape
 tagsToSearch = 'drift'
-photoPostSubtring = "is_photo post_tumblelog_"
+noOfNotes = []
+counter = 0
+avgNotes = 100
+indexOfSavedPost = []
+j = 0
+listOfNotes = []
 
-#for tag in tagsToSearch:
+#searching the tags above
 urlToSearch =  urllib.request.urlopen('https://www.tumblr.com/search/' + tagsToSearch).read()
-soup = bs.BeautifulSoup(urlToSearch, "lxml")
+soup = bs.BeautifulSoup(urlToSearch, "html.parser")
 
-for i in soup.find_all('article', {'class' : re.compile('is_photo post_tumblelog_*')}):
-#for i in soup.select('article[class*=is_photo post_]'):
-    posts = soup
+#find all posts that are images
+r = soup.find_all('article', class_="is_photo")
 
-print(posts)
+#david's code. I want it to grab just the value of data-count. It prints what I sent you.
+for article in r:
+      for tag in article.descendants:
+           if hasattr(tag, 'attrs') and 'data-count' in tag.attrs:
+               listOfNotes.append(int(tag.attrs['data-count']))
+
+
+#Seperating popular content from content we don't want
+for j in noOfNotes:
+    if (noOfNotes[j] > avgNotes):
+        indexOfSavedPost.append(j)
+        print (j,noOfNotes[j])
