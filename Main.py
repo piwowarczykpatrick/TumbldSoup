949fab23307d604d4eb938060b273e260a748864
Patrick Piwowarczyk <piwowarczykpatrick@gmail.com>
2017-03-08 02:33:50 -0600

    Added multiple tag support.

--- Main.py
+++ Main.py
@@ -4,48 +4,50 @@ import re
 import os
 
 #Input tags to analyze and scrape
-tagsToSearch = 'Drift'
+tagsToSearch = ['Drift','Streeto','180sx','jzx100','jzx110','326power']
 avgNotes = 100
 indexOfPopularPosts = []
 listOfNotes = []
 imgLinks = []
 total = 0
 
-#searching the tags above
-urlToSearch =  urllib.request.urlopen('https://www.tumblr.com/search/' + tagsToSearch).read()
-soup = bs.BeautifulSoup(urlToSearch, "html.parser")
+for tagSearch in range(len(tagsToSearch)):
+    print('Crawling: ' +tagsToSearch[tagSearch])
+    #searching the tags above
+    urlToSearch =  urllib.request.urlopen('https://www.tumblr.com/search/' + tagsToSearch[tagSearch]).read()
+    soup = bs.BeautifulSoup(urlToSearch, "html.parser")
 
-#find all posts that are images
-r = soup.find_all('article', class_="is_photo")
+    #find all posts that are images
+    r = soup.find_all('article', class_="is_photo")
 
-#david's code. I want it to grab just the value of data-count. It prints what I sent you.
-for article in r:
-      for tag in article.descendants:
-           if hasattr(tag, 'attrs') and 'data-count' in tag.attrs:
-               listOfNotes.append(int(tag.attrs['data-count']))
-#print(listOfNotes)
-
-
-#Seperating popular content from content we don't want
-for j in range(len(listOfNotes)):
-    if (listOfNotes[j] > avgNotes):
-        indexOfPopularPosts.append(j)
-        #print (j,listOfNotes[j])
+    #david's code. I want it to grab just the value of data-count. It prints what I sent you.
+    for article in r:
+          for tag in article.descendants:
+               if hasattr(tag, 'attrs') and 'data-count' in tag.attrs:
+                   listOfNotes.append(int(tag.attrs['data-count']))
+    #print(listOfNotes)
 
 
-#Populating list of popular images in the same order as the amount of likes each photo has
-allImages = soup.find_all('img')
-for img in allImages:
-    if hasattr(img, 'attrs') and 'src' in img.attrs:
-        if (str(img.attrs['src'])[0:10] == ('https://68')):
-            imgLinks.append(str(img.attrs['src']))
-print(imgLinks)
+    #Seperating popular content from content we don't want
+    for j in range(len(listOfNotes)):
+        if (listOfNotes[j] > avgNotes):
+            indexOfPopularPosts.append(j)
+            #print (j,listOfNotes[j])
 
 
-#Downloading Popular Content
-for i in range(len(listOfNotes)):
-    if (listOfNotes[i] > avgNotes):
-        DownloadLink = (str(imgLinks[i]))
-        FullFileName = os.path.join('Photos', (tagsToSearch+ '_'+str(total)+'.gif'))
-        urllib.request.urlretrieve(DownloadLink, FullFileName)
-        total += 1
+    #Populating list of popular images in the same order as the amount of likes each photo has
+    allImages = soup.find_all('img')
+    for img in allImages:
+        if hasattr(img, 'attrs') and 'src' in img.attrs:
+            if (str(img.attrs['src'])[0:10] == ('https://68')):
+                imgLinks.append(str(img.attrs['src']))
+    print(imgLinks)
+
+
+    #Downloading Popular Content
+    for i in range(len(listOfNotes)):
+        if (listOfNotes[i] > avgNotes):
+            DownloadLink = (str(imgLinks[i]))
+            FullFileName = os.path.join('Photos', (tagsToSearch[tagSearch]+ '_'+str(total)+'.gif'))
+            urllib.request.urlretrieve(DownloadLink, FullFileName)
+            total += 1
